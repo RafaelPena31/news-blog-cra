c3173b3dfc6aba3fea392870b2129c07ca8e10f9
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-11-17 19:36:55 -0300

    add news data

--- src/core/constants/news.ts
+++ src/core/constants/news.ts
@@ -1,113 +1,156 @@
 export const news = {
   health: [
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "0",
+      title:
+        "O novo tratamento personalizado para os 4 tipos de obesidade, segundo especialistas dos EUA",
+      content:
+        "Longa pesquisa em instituição americana identificou quatro 'fenótipos' da obesidade, sugerindo tratamento personalizado para cada um deles.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/12011/production/_121554737_gettyimages-157287734.jpg",
+      newsPath: "https://www.bbc.com/portuguese/geral-59314836",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "1",
+      title:
+        "É justo que quem recusou vacina contra covid seja impedido de trabalhar?",
+      content:
+        "Muitos países estão olhando para os programas de mandato de vacina dos EUA e se perguntando se as vacinas deveriam ser tornadas obrigatórias para muitos trabalhadores.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/F19E/production/_121445816_337038d2-8d79-4164-a267-2adf987ffc67.jpg",
+      newsPath: "https://www.bbc.com/portuguese/internacional-59301538",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "2",
+      title:
+        "Vacina da Janssen: como vai funcionar reforço para quem tomou 'dose única' contra covid",
+      content:
+        "O Ministério da Saúde anunciou que todos os brasileiros adultos poderão tomar um reforço cinco meses depois da segunda dose",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/12DA7/production/_117332277__117324910_gettyimages-1230141584-1.jpg",
+      newsPath: "https://www.bbc.com/portuguese/brasil-59314178",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "3",
+      title:
+        "Imunidade da vacina de covid cai com tempo: quem deve se preocupar mais?",
+      content:
+        "Entenda como o nosso corpo responde a infecções, com a ajuda da vacina, e como é natural que essa proteção diminua com o passar do tempo",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/35FF/production/_121532831_1200-xx-gettyimages-1352439415.jpg",
+      newsPath: "https://www.bbc.com/portuguese/geral-59284668",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "4",
+      title:
+        "Câncer por HPV despenca com vacina: descubra sintomas e onde se imunizar",
+      content:
+        "Estudo apontou que imunização levou a queda de quase 90% nos casos de tumor no colo do útero entre mulheres vacinadas no início da adolescência.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/51B2/production/_121541902_davipinheiro-hospitalsojos-divulgao.jpg",
+      newsPath: "https://www.bbc.com/portuguese/brasil-59269268",
     },
   ],
   politic: [
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "5",
+      title:
+        "Como viagens de Bolsonaro e Lula ao exterior antecipam 'queda de braço' eleitoral",
+      content:
+        "Especialistas afirmam que viagens dos dois ao exterior miram tanto o público externo quanto o eleitorado brasileiro.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/1183A/production/_121583717_bolsonaro-lula.jpg",
+      newsPath: "https://www.bbc.com/portuguese/brasil-59314235",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "6",
+      title:
+        "Aliado de Trump e Bolsonaro, Steve Bannon é liberado após entregar passaporte a autoridades americanas",
+      content:
+        "Estrategista da direita não depôs nem entregou documentos pedidos pelo Congresso americano, pro isso foi denunciado por desacato. Parlamentares investigam sua ligação com invasão do Capitólio.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/9D6A/production/_121089204_gettyimages-1228129433.jpg",
+      newsPath: "https://www.bbc.com/portuguese/internacional-59270676",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "7",
+      title:
+        "'Mudanças em Cuba são imparáveis', diz líder de manifestação por mais liberdade na ilha",
+      content:
+        "Para o governo cubano, movimento Arquipélago, cujo um dos líderes é o artista Yunior García, está sendo 'organizado e financiado pelos Estados Unidos'",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/230E/production/_121547980_gettyimages-1236528700.jpg",
+      newsPath: "https://www.bbc.com/portuguese/internacional-59283038",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "8",
+      title:
+        "Ministro do Meio Ambiente defende mineração e se nega a reconhecer erros por alta de desmatamento",
+      content:
+        "Em entrevista à BBC News Brasil, Leite se recusou a responder se governo vai retirar apoio a projetos no Congresso que ampliariam desmatamento e não reconheceu erros na política ambiental que levaram à piora em todos os índices ambientais durante o governo Bolsonaro.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/11A0B/production/_121530227_0e686a49-051d-4f96-980b-efc0418b3ca2.jpg",
+      newsPath: "https://www.bbc.com/portuguese/internacional-59276958",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "9",
+      title:
+        "8 pontos para entender crise com milhares de imigrantes em fronteira da União Europeia",
+      content:
+        "Para a Polônia, a União Europeia e a Organização do Tratado do Atlântico Norte (Otan), Belarus está fabricando e orquestrando a crise migratória, uma acusação negada por seu contestado líder",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/16B5D/production/_121512039_gettyimages-1236476125.jpg",
+      newsPath: "https://www.bbc.com/portuguese/internacional-59276098",
     },
   ],
   technology: [
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "10",
+      title:
+        "Golpe do Pix: hackers contam como enganam vítimas; saiba como se proteger",
+      content:
+        "De acordo especialista ouvido pela reportagem, a Receita Federal bloqueou, até setembro de 2021, mais 2,7 milhões de tentativas de golpes envolvendo o Pix.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/4391/production/_121479271_hacker1.jpg",
+      newsPath: "https://www.bbc.com/portuguese/brasil-59171217",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "11",
+      title:
+        "'Aluno dividia celular com dois irmãos': 51% na rede pública ainda não têm acesso a computador com internet",
+      content:
+        "Pesquisa exclusiva do Datafolha mostra que conectividade precária continua a ser um problema para alunos mais vulneráveis após 18 meses de pandemia",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/1215B/production/_121457047_gettyimages-1346356018.jpg",
+      newsPath: "https://www.bbc.com/portuguese/brasil-59182968",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "12",
+      title:
+        "A polêmica empresa que 'escuta' tiros nas ruas dos EUA e alerta a polícia",
+      content:
+        "A BBC teve acesso à ShotSpotter, a polêmica empresa americana de tecnologia que detecta tiros.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/D57E/production/_121245645_screenshot2021-10-27at16.57.44.png",
+      newsPath: "https://www.bbc.com/portuguese/internacional-59185447",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "13",
+      title: "Por que Elon Musk pode vender US$ 21 bi em ações da Tesla",
+      content:
+        "Após votação no Twitter, o homem mais rico do mundo prometeu se desfazer de 10% de sua participação, mas analistas apontam que ele já teria que fazer isso para pagar impostos de um enorme ganho que terá com papéis da companhia.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/4BF5/production/_121454491_mediaitem121454490.jpg",
+      newsPath: "https://www.bbc.com/portuguese/geral-59201895",
     },
     {
-      id: "",
-      title: "",
-      content: "",
-      imageUri: "",
-      newsPath: "",
+      id: "14",
+      title: "Cidades flutuantes são o futuro da humanidade?",
+      content:
+        "Assentamentos que flutuam sobre na água têm sido promovidos como uma possível solução para a elevação do nível do mar e às mudanças climáticas.",
+      imageUri:
+        "https://ichef.bbci.co.uk/live-experience/cps/800/cpsprodpb/17792/production/_121464169_f3df15ed-78d1-476f-a5a3-e167a48bc5f6.png",
+      newsPath: "https://www.bbc.com/portuguese/internacional-59141506",
     },
   ],
 };
